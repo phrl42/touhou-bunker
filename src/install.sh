3c03707a81b32423f3d5658c9262c7b6a3635df9
phrl42 <pherelohd@protonmail.com>
2021-04-29 00:16:48 +0200

    added music

--- src/install.sh
+++ src/install.sh
@@ -2,6 +2,6 @@
 
 echo "Installing Touhou-Bunker..."
 
-g++ main.cpp -o game -lSDL2 -lSDL2_image
+g++ main.cpp -o game -lSDL2 -lSDL2_image -lSDL2_mixer
 
 echo "Succesfully installed, run the game by running ./game"
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 
 int main(){
 
@@ -10,6 +11,8 @@ int main(){
   SDL_Surface *imageSurface = nullptr;
   bool menu = true;
   SDL_Event ev;
+  //Mix_Music *bgm = Mix_LoadMUS("h.mp3");
+  
   /*This is for keyboard handling stuff*/
   
   bool KEYS[322];  /* 322 SDLK_DOWN events*/
@@ -22,9 +25,16 @@ int main(){
 
 
   std::cout << "Don't mind all the logging." << std::endl;
-
-  if(SDL_Init(SDL_INIT_VIDEO) < 0)
+  /*loading video*/
+  if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
     std::cout << "Video Initialization error: " << SDL_GetError() << std::endl;
+   /*loading audio*/
+  
+  if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    std::cout << "Audio Initialization error: " << Mix_GetError() << std::endl;
+  
+  
+  
 
   else{ /*if the video was able to be initialized*/
 
@@ -43,7 +53,9 @@ int main(){
       /*window creation*/
       windowSurface = SDL_GetWindowSurface(window);
       imageSurface = SDL_LoadBMP("menu.bmp");
-      /*if the image wasn't able to be loaded*/
+      /*load the music*/
+      Mix_Music *bgm = Mix_LoadMUS("test.mp3");
+    /*if the image wasn't able to be loaded*/
       if(imageSurface == NULL)
         std::cout << "Image loading failed: " << SDL_GetError() << std::endl;
 
@@ -79,12 +91,23 @@ int main(){
                         break;
                     case SDLK_q:
 		      menu = false;
-		      std::cout << "do you see this" << std::endl;
+		      std::cout << "quitting menu..." << std::endl;
 		        break;
 		    case SDLK_RETURN:
 		      std::cout << "Entering game..." << std::endl;
 		      /*todo*/
-		      break;
+		    case SDLK_p:
+
+		    std::cout << "playing music" << std::endl;
+		    /*plays the music*/
+		        if(!Mix_PlayingMusic())
+			    Mix_PlayMusic(bgm, -1);
+
+			else if(Mix_PausedMusic())
+				Mix_ResumeMusic();
+			else
+			    Mix_PauseMusic();
+			break;	
 		    default:
                         break;
 	       
@@ -101,6 +124,8 @@ int main(){
     /*free the allocated memory*/
   SDL_FreeSurface(imageSurface);
   SDL_DestroyWindow(window);
+ /* Mix_FreeMusic(bgm);  ignore this for now*/
+  Mix_Quit();
   SDL_Quit();
 
     return 0;
