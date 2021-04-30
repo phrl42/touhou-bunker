75e319d608a7986d3d07bfcbff1d5056a84f4adc
phrl42 <pherelohd@protonmail.com>
2021-04-30 20:29:23 +0200

    cleaned up the code

--- src/main.cpp
+++ src/main.cpp
@@ -11,32 +11,30 @@ int main(){
   SDL_Surface *imageSurface = nullptr;
   bool menu = true;
   SDL_Event ev;
-  //Mix_Music *bgm = Mix_LoadMUS("h.mp3");
   
   /*This is for keyboard handling stuff*/
   
   bool KEYS[322];  /* 322 SDLK_DOWN events*/
 
-  for(int i = 0; i < 322; i++) { /* all events to false*/
+  for(int i = 0; i < 322; i++)  /* all events to false*/
     KEYS[i] = false;
-}
 
-  //  SDL_EnableKeyRepeat(0,0); /*check for holding the key down*/
-
-
+  
   std::cout << "Don't mind all the logging." << std::endl;
+
   /*loading video*/
+
   if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
     std::cout << "Video Initialization error: " << SDL_GetError() << std::endl;
-   /*loading audio*/
+
+  /*loading audio*/
   
   if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
     std::cout << "Audio Initialization error: " << Mix_GetError() << std::endl;
   
-  
-  
+  /*if the video was able to be initialized*/
 
-  else{ /*if the video was able to be initialized*/
+  else{ 
 
     /*window parameters*/
     window = SDL_CreateWindow("Touhou-Bunker",
@@ -44,80 +42,93 @@ int main(){
                         SDL_WINDOWPOS_CENTERED,
                         640, 480,
                         SDL_WINDOW_SHOWN);
-    /*if the window suddenly dies*/
+    
+    /*check if the window dies*/
+
   }if(!window)
     std::cout<< "Window Initialization failed: " << SDL_GetError() << std::endl;
 
+  else{
+
+    /*window creation*/
+
+    windowSurface = SDL_GetWindowSurface(window);
+    imageSurface = SDL_LoadBMP("menu.bmp");
+
+    /*load the bgm*/
+
+    Mix_Music *bgm = Mix_LoadMUS("test.mp3");
+
+    /*if the image wasn't able to be loaded*/
+
+    if(imageSurface == NULL)
+      std::cout << "Image loading failed: " << SDL_GetError() << std::endl;
+
     else{
 
-      /*window creation*/
-      windowSurface = SDL_GetWindowSurface(window);
-      imageSurface = SDL_LoadBMP("menu.bmp");
-      /*load the music*/
-      Mix_Music *bgm = Mix_LoadMUS("test.mp3");
-    /*if the image wasn't able to be loaded*/
-      if(imageSurface == NULL)
-        std::cout << "Image loading failed: " << SDL_GetError() << std::endl;
+      /*play the music in a loop*/
 
-      else{
-	/*play the music in a loop*/
-	if(!Mix_PlayingMusic())
-	  Mix_PlayMusic(bgm, -1);
-        /*the menu loop, only exits on demand (killing the program)*/
-        while(menu){ 
+      if(!Mix_PlayingMusic())
+	Mix_PlayMusic(bgm, -1); /*the -1 is for an infinite loop*/
 
-	  /*this is the menu*/
+      /*the menu loop, only exits on demand (killing the program)*/
+
+      while(menu){ 
+
+	/*this is the menu (no shit) */
 	
         /*load the bmp and draw it onto the windowSurface*/
-        SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+
+	SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
         SDL_UpdateWindowSurface(window);
 
-          while(SDL_PollEvent(&ev) != 0){
+	while(SDL_PollEvent(&ev) != 0){
 		  
-	    switch(ev.type){
+	  switch(ev.type){
             /*Look for a keypress*/
-            case SDL_KEYDOWN:
-                /*Check the SDLKey values*/
-                switch(ev.key.keysym.sym){
+	  case SDL_KEYDOWN:
+	    /*Check the SDLKey values*/
+	    switch(ev.key.keysym.sym){
 		  
-                    case SDLK_LEFT:
-		      std::cout << "left press" << std::endl;
-                        break;
-                    case SDLK_RIGHT:
-		      std::cout << "right press" << std::endl;
-                        break;
-                    case SDLK_UP:
-		      std::cout << "up press" << std::endl;
-                        break;
-                    case SDLK_DOWN:
-		      std::cout << "down press" << std::endl;
-                        break;
-                    case SDLK_q:
-		      menu = false;
-		      std::cout << "quitting menu..." << std::endl;
-		        break;
-		    case SDLK_RETURN:
-		      std::cout << "Entering game..." << std::endl;
-		      /*todo*/	
-		    default:
-                        break;
+	    case SDLK_LEFT:
+	      std::cout << "left press" << std::endl;
+	      break;
+	    case SDLK_RIGHT:
+	      std::cout << "right press" << std::endl;
+	      break;
+	    case SDLK_UP:
+	      std::cout << "up press" << std::endl;
+	      break;
+	    case SDLK_DOWN:
+	      std::cout << "down press" << std::endl;
+	      break;
+	    case SDLK_q:
+	      menu = false;
+	      std::cout << "quitting menu..." << std::endl;
+	      break;
+	    case SDLK_RETURN:
+	      std::cout << "entering game..." << std::endl;
+	      MixQuit();
+	      mainGame();
+	      /*todo*/	
+	    default:
+	      break;
 	       
-                }
-		/*there are 2 switch statements, so don't forget to break twice!*/
-		  if(menu == false)
-		    break;
-            }
-        
-          }
-        }
-   }
- }
-    /*free the allocated memory*/
+	    }
+	    /*there are 2 switch statements, so don't forget to break twice!(it was painful to find it out)*/
+	    if(menu == false)
+	      break;
+	  }	  
+	}
+      }
+    }
+  }
+  /*free the allocated memory*/
   SDL_FreeSurface(imageSurface);
   SDL_DestroyWindow(window);
- /* Mix_FreeMusic(bgm);  ignore this for now*/
+  /* Mix_FreeMusic(bgm);  ignore this for now*/
   Mix_Quit();
   SDL_Quit();
 
-    return 0;
- }
+  return 0;
+}
