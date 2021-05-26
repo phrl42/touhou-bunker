5ec1c79f82fa0669eb980bf53361e8bf4bdbb3f2
HOSTINGAHMD <ahmed.hussain2080@gmail.com>
2021-05-27 00:53:25 +0500

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -4,4 +4,4 @@ echo "Installing Touhou-Bunker..."
 
 g++ main.cpp -o game -lSDL2 -lSDL2_image -lSDL2_mixer
 
-echo "Successfully installed, run the game by running ./game"
+echo "Successfully installed, run the game by executing ./game"
