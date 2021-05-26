a746fa420b896f4508baadbda18bde73cd086d60
phrl42 <44616889+phrl42@users.noreply.github.com>
2021-05-26 20:15:46 +0000

    f

--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Installing Touhou-Bunker..."
+echo "Compiling touhou-bunker..."
 
 g++ main.cpp -o game -lSDL2 -lSDL2_image -lSDL2_mixer
 
-echo "Successfully installed, run the game by running ./game"
+echo "If there are no errors above, the game installed itself successfully. Execute it by running: ./game"
