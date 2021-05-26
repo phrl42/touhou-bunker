5c9d365d8df1aadc46d283cea56c35e5fdfaad5a
HOSTINGAHMD <ahmed.hussain2080@gmail.com>
2021-05-27 01:20:33 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
 
-int main(){
+int main() {
 
   //window and surface initialization
   SDL_Window *window = nullptr;
@@ -12,83 +12,83 @@ int main(){
   bool menu = true;
   SDL_Event ev;
   
-  //This is for keyboard handling stuff
+  // This is for keyboard handling stuff
   
-  bool KEYS[322];  //322 SDLK_DOWN events
+  bool KEYS[322];  // 322 SDLK_DOWN events
 
-  for(int i = 0; i < 322; i++)  //set all events to false
+  for(int i = 0; i < 322; i++)  // set all events to false
     KEYS[i] = false;
 
   
   std::cout << "Don't mind all the logging." << std::endl;
 
-  //loading video
+  // loading video
 
   if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
     std::cout << "Video Initialization error: " << SDL_GetError() << std::endl;
 
-  //loading audio
+  // loading audio
   
   if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
     std::cout << "Audio Initialization error: " << Mix_GetError() << std::endl;
   
-  //if the video was able to be initialized
+  // if the video was able to be initialized
 
   else{ 
 
-    //window parameters
+    // window parameters
     window = SDL_CreateWindow("Touhou-Bunker",
                         SDL_WINDOWPOS_CENTERED,
                         SDL_WINDOWPOS_CENTERED,
                         640, 480,
                         SDL_WINDOW_SHOWN);
     
-    //check if the window dies
+    // check if the window dies
 
   }if(!window)
     std::cout<< "Window Initialization failed: " << SDL_GetError() << std::endl;
 
-  else{
+  else {
 
-    //window creation
+    // window creation
 
     windowSurface = SDL_GetWindowSurface(window);
     imageSurface = SDL_LoadBMP("img/menu.bmp");
 
-    //load the bgm
+    // load the bgm
 
     Mix_Music *bgm = Mix_LoadMUS("bgm/test.mp3");
 
-    //if the image wasn't able to be loaded
+    // if the image wasn't able to be loaded
 
     if(imageSurface == NULL)
       std::cout << "Image loading failed: " << SDL_GetError() << std::endl;
 
-    else{
+    else {
 
-      //play the music in a loop
+      // play the music in a loop
 
       if(!Mix_PlayingMusic())
-	Mix_PlayMusic(bgm, -1); //the -1 is for an infinite loop
+	Mix_PlayMusic(bgm, -1); // the -1 is for an infinite loop
 
-      //the menu loop, only exits on demand (killing the program)
+      // the menu loop, only exits on demand (killing the program)
 
-      while(menu){ 
+      while(menu) { 
 
-	//this is the menu (no shit) 
+	// this is the menu (no shit) 
 	
-        //load the bmp and draw it onto the windowSurface
+        // load the bmp and draw it onto the windowSurface
 
 	SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
         SDL_UpdateWindowSurface(window);
 
-	while(SDL_PollEvent(&ev) != 0){
+	while(SDL_PollEvent(&ev) != 0) {
 		  
-	  switch(ev.type){
-            //Look for a keypress
+	  switch(ev.type) {
+            // Look for a keypress
 	  case SDL_KEYDOWN:
-	    //Check the SDLKey values
-	    switch(ev.key.keysym.sym){
+	    // Check the SDLKey values
+	    switch(ev.key.keysym.sym) {
 		  
 	    case SDLK_LEFT:
 	      std::cout << "left" << std::endl;
@@ -177,10 +177,10 @@ int main(){
       }
     }
   }
-  //free the allocated memory
+  // free the allocated memory
   SDL_FreeSurface(imageSurface);
   SDL_DestroyWindow(window);
-  //Mix_FreeMusic(bgm);  ignore this for now
+  // Mix_FreeMusic(bgm);  ignore this for now
   Mix_Quit();
   SDL_Quit();
 
