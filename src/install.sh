ac86b41ecd00b15e7734d27a189f8f545f346f7b
rnb <rnbwastaken@tutanota.com>
2021-05-26 22:05:16 +0200

    Updated install.sh!!!! FIX THIS!!!!

--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Installing Touhou-Bunker..."
+echo "Compiling Touhou-Bunker..."
 
 g++ main.cpp -o game -lSDL2 -lSDL2_image -lSDL2_mixer
 
-echo "Successfully installed, run the game by running ./game"
+echo "If you don't see any errors above, you have successfully compiled a binary for touhou-bunker! Start the game by executing ./game"
