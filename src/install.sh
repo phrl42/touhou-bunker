d830dc1e3b4f7fd88e69aaae195abd538600ee75
phrl42 <phrl42@riseup.net>
2022-02-19 10:47:24 +0100

    added makefile

--- src/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Compiling touhou-bunker..."
-
-g++ main.cpp -o game -lSDL2 -lSDL2_image -lSDL2_mixer
-
-echo "If there are no errors above, the game installed itself successfully. Execute it by running: ./game"
-# Thanks for reminding me, rnbunker!
